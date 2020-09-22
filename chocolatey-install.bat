87901170429ef025447dcf71c1c5a16a3d673fed
Jeong Min Lim <limjeongmin@wustl.edu>
2020-09-22 12:24:03 -0500

    Create chocolatey-install.bat

---
+++ chocolatey-install.bat
@@ -0,0 +1,7 @@
+:: Chocolatey install script
+
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:: Install all the packages in packages.txt
+
+for /F "eol=#" %%G in (packages.txt) do choco install %%G -fy
